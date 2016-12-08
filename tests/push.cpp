f078dd31c839e6f2c8e7dc47df064f188dfb17b2
Anastasia <vernidub.anastasya@yandex.ru>
2016-12-08 07:51:56 +0300

    Update push.cpp

--- tests/push.cpp
+++ tests/push.cpp
@@ -9,10 +9,10 @@ SCENARIO("Incremented number of elements after pushing one to the stack")
         stack<int> st1;
         st1.push(10);
         st1.push(4);
-        auto count = st.count();
+        auto count = st1.count();
         WHEN("pushing")
         {
-            st.push(43);
+            st1.push(43);
             THEN("Number of elements must be incremented by 1")
             {
                 REQUIRE(st1.count() == count + 1);
