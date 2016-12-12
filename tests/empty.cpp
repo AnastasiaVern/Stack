c7fe2a322d9810698bf17016cba11afeeb157448
Anastasia <vernidub.anastasya@yandex.ru>
2016-12-13 00:04:52 +0300

    Create empty.cpp

---
+++ tests/empty.cpp
@@ -0,0 +1,32 @@
+#include "catch.hpp"
+#include <stack.hpp>
+
+SCENARIO("If stack is empty")
+{
+    GIVEN("Empty stack")
+    {
+        stack<int> st1;
+        WHEN("st1.empty()")
+        {
+            THEN("return true")
+            {
+                REQUIRE(st1.empty() == true);
+            }
+        }
+    }
+
+    GIVEN("If stack is full")
+    {
+        stack<int> st1;
+        st1.push(10);
+        st1.push(20);
+        st1.push(30);
+        WHEN("st1.empty()")
+        {
+            THEN("return false")
+            {
+                REQUIRE(st1.empty() == false);
+            }
+        }
+    }
+}
