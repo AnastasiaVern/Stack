49ac4c09c9aa3c842ddb999500d09419cf092b73
Anastasia <vernidub.anastasya@yandex.ru>
2016-12-14 23:45:36 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -13,11 +13,6 @@ public:
 	auto top() const->T; 
 	auto pop() noexcept -> void;
 	auto empty() const noexcept-> bool;
-
-/*private:
-	T * array_;
-	size_t array_size_;
-	size_t count_;*/
 };
 
 template <typename T>
