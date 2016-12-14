01116f3ef323c5965f4296678a38076990e6813a
Anastasia <vernidub.anastasya@yandex.ru>
2016-12-14 23:45:04 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,121 +1,60 @@
+#include "stdafx.h"
+#include "Allocator.hpp"
 #include <iostream>
 #include <exception>
+#include <algorithm>
 template <typename T>
-class stack
+class stack : public allocator<T>
 {
 public:
-	stack();
-	auto count() const noexcept -> size_t; 
-	auto push(T const& value)-> void; /*strong*/
-	auto top() const-> T; /*strong*/
+	stack() : allocator<T>(5) {};
+	auto count() const noexcept->size_t;
+	auto push(allocator<T> &obj,T const& value)-> void;
+	auto top() const->T; 
 	auto pop() noexcept -> void;
 	auto empty() const noexcept-> bool;
-	auto print_stack() const noexcept->void;
-	~stack();
-private:
+
+/*private:
 	T * array_;
 	size_t array_size_;
-	size_t count_;
+	size_t count_;*/
 };
-
-template <typename T>
-stack<T>::stack() : array_(0), array_size_(0), count_(0) {};
 
 template <typename T>
 auto stack<T>::count() const noexcept -> size_t
 {
-	return count_;
+	return allocator<T>::count_;
 }
 
-template <typename T> 
-auto stack<T>::push(T const& value)->void /*strong*/
+template <typename T>
+auto stack<T>::push(allocator<T> &obj,T const& value)->void 
 {
-    bool any_change= false;
-    T* old_array = array_;
-    T* new_array = nullptr;
-
-    if (count_ == array_size_)
-    {
-        array_size_ *= 2;
-        try
-        {
-            new_array = new T[array_size_];
-       while (array_ != array_ + count_) 
-       {
-				*new_array++ = *array_++;
-			}
-        }
-        catch (...)
-        {
-            delete[] new_array;
-            array_size_ /= 2;
-            throw;
-        }
-        array_ = new_array;
-        any_change = true;
-    }
-
-    try
-    {
-        array_[count_] = value;
-    }
-    catch (...)
-    {
-        if (any_change)
-        {
-            delete[] array_;
-            array_size_ /= 2;
-            array_ = old_array;
-        }
-        throw;
-    }
-	
-    ++count_;
-    if (any_change)  
-    {
-	    delete[] old_array;
-    }
-    return;
-
+	 allocator<T>::swap(obj);
+     allocator<T>::ptr_[allocator<T>::count_] = value;
+	 allocator<T>::count_++;
 }
 template <typename T>
 auto stack<T>::top() const -> T
 {
-	if (count_ != 0)
+	if (allocator<T>::count_)
 	{
-	      return array_[count_-1];
+		return allocator<T>::ptr_[allocator<T>::count_ - 1];
 	}
 	else
-	{ 
+	{
 		throw std::underflow_error("count_ = 0");
 	}
 }
 template <typename T>
 auto stack<T>::pop() noexcept->void
 {
-	if (count_ != 0) {
-		--count_;
-	}
-}
-
-template <typename T>
-stack<T>::~stack()
-{
-	delete[] array_;
+	if (allocator<T>::count_)
+		allocator<T>::count_--;
 }
 
 template<typename T>
-auto stack<T>::print_stack() const noexcept->void 
-{
-	for (size_t i = 0; i < count_; ++i) 
-	{
-		std::cout << array_[i] <<" ";
-	}
-	std::cout << std::endl;
-}
-template<typename T> 
 auto stack<T>::empty() const noexcept->bool
 {
-	if (!count_) { return true; }
+	if (!allocator<T>::count_;) { return true; }
 	else { return false; }
 }
