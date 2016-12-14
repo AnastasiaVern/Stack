1ba5ee5f24f6d5e14f6844d83a0f0e39b0b372bd
Anastasia <vernidub.anastasya@yandex.ru>
2016-12-14 23:44:39 +0300

    Create allocator.cpp

---
+++ include/allocator.cpp
@@ -0,0 +1,58 @@
+#include <exception>
+#include <algorithm>
+#include <iostream>
+
+
+template <typename T>
+class allocator
+{
+public:
+	allocator(size_t size = 0);
+	auto swap(allocator& other) -> void;
+	allocator(allocator const& alloc) noexcept= delete;
+	auto operator =(allocator const& )->allocator& = delete;
+	~allocator();
+
+	T * ptr_;
+	size_t size_;
+	size_t count_;
+};
+
+
+template<typename T>
+allocator<T>::allocator(size_t size) 
+{
+	ptr_ = static_cast<T*>(::operator new(size * sizeof(T)));
+	size_=size;
+	count_ = 0;
+}
+
+template<typename T>
+auto allocator<T>::swap(allocator& other) -> void 
+{
+	try 
+	{
+		size_ * 2;
+		other.size_;
+		while (ptr_!= ptr_ + count_)
+		{
+			*other.ptr_++ = *ptr_++;
+		}
+		other.count_ = count_;
+		std::swap(ptr_, other.ptr_);
+		std::swap(size_, other.size_);
+	}
+	catch (...) 
+	{
+		std::cerr << "error!";
+	}
+	
+}
+
+template<typename T>
+allocator<T>::~allocator() 
+{
+	for (size_t i = 0; i < count_; i++)
+		ptr_[i].~T();
+	::operator delete[](ptr_);
+}
