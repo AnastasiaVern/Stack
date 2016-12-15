d2720f58b7a9bdde1141b94f03c3160eed14c533
Anastasia <vernidub.anastasya@yandex.ru>
2016-12-15 11:53:32 +0300

    Update pop.cpp

--- tests/pop.cpp
+++ tests/pop.cpp
@@ -1,26 +1,6 @@
 #include "catch.hpp"
 #include <stack.hpp>
 
-SCENARIO("Decremented number of elements after popping one from the stack")
-{
-    GIVEN("stack")
-    {
-        stack<int> st1;
-        st1.push(2);
-        st1.push(3);
-        st1.push(4);
-        st1.push(5);
-        WHEN("popping")
-        {
-            st1.pop();
-            THEN("Number of elements must be decremented by one")
-            {
-                REQUIRE(st1.count() == 3);
-                REQUIRE(st1.top()==4);
-            }
-        }
-    }
-}
 SCENARIO("If stack is empty when popping") {
     GIVEN("Empty stack")
     {
