0e6a1ae016a9b70c774bed0940b675eb6cd3e7a0
Anastasia <vernidub.anastasya@yandex.ru>
2016-12-15 11:51:43 +0300

    Update top.cpp

--- tests/top.cpp
+++ tests/top.cpp
@@ -3,20 +3,6 @@
 
 SCENARIO("Top() returns top element")
 {
-    GIVEN("Stack")
-    {
-        stack<int> st1;
-        st1.push(1);
-        st1.push(2);
-        st1.push(3);
-        WHEN("st1.top()")
-        {
-            THEN("st1.top() must return 3")
-            {
-                REQUIRE(st1.top() == 3);
-            }
-        }
-    }
     GIVEN("If stack is completely empty")
     {
         stack<int> st1;
